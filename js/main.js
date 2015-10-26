76ef0934eb31aebc91fd25c93c2ada0e4ad416dd
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-26 13:56:15 -0400

    It WORKS!!

--- js/main.js
+++ js/main.js
@@ -2,4 +2,27 @@ import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
 
-console.log('Hello, World');
+$.ajaxSetup({
+  headers:{
+    'X-Parse-Application-Id': 'HLyprOtO04VKlQ0i2G51lTHwb1a2zDjOjhbjMDvR',
+    'X-Parse-REST-API-Key': 'PvuKW4U4Kd0RLix0G0IOG3IYlJVShxV4ctjJVMlP'
+  }
+});
+
+import MovieCollection from './movie_collection';
+import MovieMaker from './movie_maker';
+
+let movie = new MovieCollection();
+
+function renderMovie(){
+  let $ul = $('<ul></ul>');
+  movie.each(function(film){
+    let data = film.toJSON();
+    let $li = $(MovieMaker(data));
+    $ul.append($li);
+  });
+  $('body').html($ul);
+}
+movie.fetch().then(renderMovie);
+
+console.log(renderMovie);
--- js/movie_maker.js
+++ js/movie_maker.js
@@ -1,5 +1,5 @@
 function MovieMaker(data){
-  return `<li>${data.Title} is rated ${RatingOutOf10} out of 10</li>`;
+  return `<li>${data.Title} is rated ${data.RatingOutOf10} out of 10</li>`;
 }
 
 export default MovieMaker;
